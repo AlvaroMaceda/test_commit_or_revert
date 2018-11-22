3bc241cd2f28ceb4a78d5c5b3ca1952b218935eb
chapuzzo <luismax@gmail.com>
2018-11-22 18:43:28 +0100

    1542908608

--- test/FizzBuzz.js
+++ test/FizzBuzz.js
@@ -5,10 +5,19 @@ describe('it', () => {
   it('works', () => {
     expect(true).to.eq(true)
   })
+
   it('returns 1 when 1', () =>  {
     expect(fizzbuzz(1)).to.eq('1');
   })
+
   it('returns 78 when 78', () => {
     expect(fizzbuzz(78)).to.eq('78');
   })
+
+
+  it('returns Fizz when 3', () => {
+    expect(fizzbuzz(3)).to.eq('Fizz');
+  })
+
+
 })
