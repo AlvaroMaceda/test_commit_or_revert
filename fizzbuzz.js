6c1e34a0b7ab5da77c3b5a3a39fb1cd21e86d315
chapuzzo <luismax@gmail.com>
2018-11-22 18:40:27 +0100

    1542908427

---
+++ fizzbuzz.js
@@ -0,0 +1,3 @@
+module.exports = function fizzbuzz() {
+  return '1';
+}
--- test/FizzBuzz.js
+++ test/FizzBuzz.js
@@ -1,7 +1,11 @@
-const {expect} = require('chai')
+const {expect} = require('chai');
+const fizzbuzz = require('../fizzbuzz');
 
 describe('it', () => {
   it('works', () => {
     expect(true).to.eq(true)
+  })
+  it('requires', () =>  {
+    expect(fizzbuzz(1)).to.eq('1');
   })
 })
