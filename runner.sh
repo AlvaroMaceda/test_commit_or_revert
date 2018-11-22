d250184a5fc0c559403b365054110d1b753fe06c
chapuzzo <luismax@gmail.com>
2018-11-22 18:32:01 +0100

    1542907921

---
+++ runner.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+TEST_OUTPUT=/tmp/tcr_ouput.log
+
+test () {
+ npm test > $TEST_OUTPUT
+}
+
+commit () {
+ cat $TEST_OUTPUT
+ echo "Green!!!"
+ git add -A
+ git commit -m "$(date +%s)"
+}
+
+revert () {
+ echo "Revert =("
+ git reset --hard
+ git clean -fd
+}
+
+test && commit || revert
